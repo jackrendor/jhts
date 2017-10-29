813807e7ec222ba7f271490040487941339b2bbe
jackrendor <jackrendor@gmail.com>
2017-10-29 11:17:19 +0100

    Fixed time machine issue.

--- jhts.sh
+++ jhts.sh
@@ -2,7 +2,7 @@
 
 #  jhts.sh
 #
-#  Copyright 2018 Jack <jackrendor@gmail.com>
+#  Copyright 2017 Jack <jackrendor@gmail.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
